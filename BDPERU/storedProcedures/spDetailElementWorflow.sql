f59b0a5f5db53cce8b86587197edfa50af9ce85b
DESKTOP-R25F92F\jlps1197 <joseluisproanosipan@gmail.com>
2018-11-20 21:40:19 -0500

    feat(sql): adding modify SP for updating element order number

--- BDPERU/storedProcedures/spDetailElementWorflow.sql
+++ BDPERU/storedProcedures/spDetailElementWorflow.sql
@@ -1,9 +1,9 @@
 ﻿CREATE PROCEDURE [dbo].[spDetailElementWorflow]
 	@workflowId int
 AS
-	SELECT		E.id,E.name,EW.workflowId,EW.initialFlag,EW.wipFlag,EW.doneFlag
+	SELECT		EW.orderNumber,E.id,E.name,EW.workflowId,EW.initialFlag,EW.wipFlag,EW.doneFlag
 	FROM		tblElements E INNER JOIN
 				tblElementWorkflows EW ON E.id = EW.id
 	WHERE		EW.workflowId = @workflowId
-	ORDER BY	EW.doneFlag,EW.wipFlag,EW.initialFlag,E.id
+	ORDER BY	EW.orderNumber,EW.doneFlag,EW.wipFlag,EW.initialFlag
 RETURN 0
---
+++ BDPERU/storedProcedures/spModifyElementWorkflow.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[spModifyElementWorkflow]
+	@elementId int,
+	@workflowId int,
+	@initialFlag bit,
+	@wipFlag bit,
+	@doneFlag bit,
+	@orderNumber int
+AS
+	UPDATE tblElementWorkflows
+	SET		initialFlag = @initialFlag,
+			wipFlag = @wipFlag,
+			doneFlag = @doneFlag,
+			orderNumber = @orderNumber
+	WHERE	elementId = @elementId AND workflowId = @workflowId
+RETURN 0
